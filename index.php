75cea2f22c41779f2a1db898b0be0a34b4617f23
saad bhuiyan <96346026+saadalbhuiyan@users.noreply.github.com>
2025-08-22 14:49:09 +0600

    frist commit

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<?php 
-
-class car {
-    public $brand ; 
-    public $color ; 
-    public $speed ; 
-
+<?php
+class Car {
+    public $brand;
+    public $color;
+    public $speed;
 
     public function start() {
         return "{$this->brand} ({$this->color}) has started!";
@@ -21,14 +19,29 @@ class car {
     }
 }
 
-
-$car1 = new Car(); 
-$car1-> brand = 'toyota' ; 
-$car2 -> color = 'red'; 
-$car3 -> speed = 0 ; 
+$car1 = new Car();
+$car1->brand = "Toyota";
+$car1->color = "Red";
+$car1->speed = 0;
 
 echo $car1->start() . "<br>";
 echo $car1->accelerate(30) . "<br>";
 echo $car1->accelerate(20) . "<br>";
 echo $car1->stop() . "<br><br>";
-?>
+
+$car2 = new Car();
+$car2->brand = "BMW";
+$car2->color = "Black";
+$car2->speed = 0;
+
+echo $car2->start() . "<br>";
+echo $car2->accelerate(50) . "<br>";
+echo $car2->stop() . "<br><br>";
+
+$car3 = new Car();
+$car3->brand = "Tesla";
+$car3->color = "White";
+$car3->speed = 0;
+
+echo $car3->start() . "<br>";
+echo $car3->accelerate(100) . "<br>";
